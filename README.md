e4e33adec8e30c35cc821c12a269454c7c11b3a5
mikik0 <usapon_love1222@yahoo.co.jp>
2021-01-30 13:26:59 +0900

    README追記/controller作成中

---
+++ README.md
@@ -0,0 +1,8 @@
+# mokutika
+目標をみつける。宣言する。みんなで走る。
+目標を通じて人生を豊かにするアプリ！
+https://www.mokutika.live/
+
+## Languages
+* sinatra
+* ruby
---
+++ controllers/users_controller.rb
@@ -0,0 +1,14 @@
+require './models/user.rb'
+require './models/post.rb'
+require './models/category.rb'
+require './models/comment.rb'
+require './models/done.rb'
+require './models/follow.rb'
+
+class UsersController
+
+  def create
+
+  end
+
+end
